38669014f2652e8b063aa305797a4dbd0a2111d1
drusangen <boris.o.vega@gmail.com>
2018-04-17 16:27:07 -0400

    primer intento

---
+++ src/ventaZ.js
@@ -0,0 +1,143 @@
+export const ventaZ = [
+
+  {
+
+    id: 1,
+
+    name: 'Leanne Graham',
+
+    total: 0,
+
+    cancelado: 0,
+
+    deuda: 0,
+
+  },
+
+  {
+
+    id: 2,
+
+    name: 'Ervin Howell',
+
+    total: 0,
+
+    cancelado: 0,
+
+    deuda: 0,
+
+  },
+
+  {
+
+    id: 3,
+
+    name: 'Clementine Bauch',
+
+    total: 0,
+
+    cancelado: 0,
+
+    deuda: 0,
+
+  },
+
+  {
+
+    id: 4,
+
+    name: 'Patricia Lebsack',
+
+    total: 0,
+
+    cancelado: 0,
+
+    deuda: 0,
+
+  },
+
+  {
+
+    id: 5,
+
+    name: 'Chelsey Dietrich',
+
+    total: 0,
+
+    cancelado: 0,
+
+    deuda: 0,
+
+  },
+
+  {
+
+    id: 6,
+
+    name: 'Mrs. Dennis Schulist',
+
+    total: 0,
+
+    cancelado: 0,
+
+    deuda: 0,
+
+  },
+
+  {
+
+    id: 7,
+
+    name: 'Kurtis Weissnat',
+
+    total: 0,
+
+    cancelado: 0,
+
+    deuda: 0,
+
+  },
+
+  {
+
+    id: 8,
+
+    name: 'Nicholas Runolfsdottir V',
+
+    total: 0,
+
+    cancelado: 0,
+
+    deuda: 0,
+
+  },
+
+  {
+
+    id: 9,
+
+    name: 'Glenna Reichert',
+
+    total: 0,
+
+    cancelado: 0,
+
+    deuda: 0,
+
+  },
+
+  {
+
+    id: 10,
+
+    name: 'Clementina DuBuque',
+
+    total: 0,
+
+    cancelado: 0,
+
+    deuda: 0,
+
+  }
+
+];
